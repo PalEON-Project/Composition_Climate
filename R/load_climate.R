8e7a93cf009e1c571f265f8b66c0edaf0d30f437
Simon <simon.j.goring@gmail.com>
2015-06-19 22:05:07 -0500

    Minor text changes.  Changes at head of Rmd to reflect changes in the sourced files.
    Sourced files are now run as functions, keeping local variables internal so we don't have so many variables loaded globally.
    
    Removed `R/load_fia.R` and put the function into `R/load_veg.R`.

--- R/load_climate.R
+++ R/load_climate.R
@@ -5,9 +5,14 @@
 #  To crop and reproject we use the R file, included hereL
 #  'R/building_small_climate.R'
 
-clim.files <- list.files('data/input/climate', full.names = TRUE)
+load_climate <- function(){
+  clim.files <- list.files('data/input/climate', full.names = TRUE)
 
-now.rast <- stack(clim.files[regexpr('norm', clim.files)>0])
-then.rast <- stack(clim.files[regexpr('early', clim.files)>0])
+  now.rast <- stack(clim.files[regexpr('norm', clim.files)>0])
+  then.rast <- stack(clim.files[regexpr('early', clim.files)>0])
 
-bio.vals <- c('ppt', 'tmax', 'tmean', 'tmin')
+  bio.vals <- c('ppt', 'tmax', 'tmean', 'tmin')
+  return(list(now = now.rast, then = then.rast, names = bio.vals))
+}
+
+climate <- load_climate()
--- R/load_veg.R
+++ R/load_veg.R
@@ -1,22 +1,98 @@
-source('R/load_fia.R')
+get_fia <- function(x){
+  fia.trans <- read.csv('data/FIA_conversion.csv', stringsAsFactors = FALSE)
+  fia.trans[fia.trans == ''] <- NA
+  
+  #  Load FIA composition data:
+  get.fia.stack <- function(x, type){
+    stack.files <- list.files(x, full = TRUE)
+    stack.fia <- resample(stack(stack.files), pls.rast, method = 'ngb')
+    stack.fia[is.na(getValues(pls.rast))] <- NA
+    
+    fia.vals <- getValues(stack.fia)
+    name.end <- regexpr(paste('_', type, sep=''), colnames(fia.vals), fixed=TRUE)
+    colnames(fia.vals) <- gsub("_$","", substr(colnames(fia.vals), 1, name.end-1))
+    colnames(fia.vals) <- gsub('_', '.', colnames(fia.vals))
+    
+    fia.vals <- as.data.frame(fia.vals)
+    
+    fia.vals$x <- xyFromCell(stack.fia, 1:ncell(stack.fia))[,1]
+    fia.vals$y <- xyFromCell(stack.fia, 1:ncell(stack.fia))[,2]
+    
+    fia.vals
+  }
+  
+  fia.dens <- get.fia.stack('data/albers/density/', type = 'density')
+  
+  re.agg <- function(x){
+    # This takes the fia data (by species) and aggregates it to PalEON taxa.
+    orig <- 1:nrow(x)
+    
+    x.test <- x[,!colnames(x) %in% c('x', 'y')]
+    
+    x.test$orig <- orig
+    
+    x.melt <- melt(x.test, na.rm=TRUE, value.name= 'value', id = c('orig'))
+    
+    x.melt$equiv <- fia.trans$PalEON[match(gsub('.', ' ', x.melt$variable, fixed = TRUE), 
+                                           gsub('.', ' ', fia.trans$scientific, fixed = TRUE))]
+    
+    x.melt <- x.melt[x.melt$value > 0,]
+    
+    x.cast <- dcast(x.melt, orig ~ equiv, 
+                    sum, drop = FALSE, 
+                    value.var = 'value')
+    
+    x.cast$x <- x$x[x.cast$orig]
+    x.cast$y <- x$y[x.cast$orig]
+    
+    x.cast$cell <- extract(setValues(unit.raster, 1:ncell(unit.raster)), x.cast[,c('x', 'y')])
+    
+    x.cast <- x.cast[!is.na(x.cast$cell),!is.na(colnames(x.cast))]
+    x.melt_2 <- melt(x.cast[ ,!colnames(x.cast)%in% c('orig')], 
+                     na.rm = TRUE, 
+                     value.name= 'value', 
+                     id = c('cell'))
+    
+    x.melt_2 <- x.melt_2[x.melt_2$value > 0,]
+    
+    x.cast <-  dcast(x.melt_2, 
+                     cell ~ variable, 
+                     mean, 
+                     drop = FALSE, 
+                     value.var = 'value')
+    
+    if(any(! unique(fia.trans$PalEON) %in% colnames(x.cast))){
+      missing <- unique(fia.trans$PalEON)[!unique(fia.trans$PalEON) %in% colnames(x.cast) &
+                                            !is.na(unique(fia.trans$PalEON))]
+      new.cols <- length(missing)
+      append.it <- matrix(ncol = new.cols, nrow = nrow(x.cast))
+      colnames(append.it) <- missing
+      x.cast <- cbind(x.cast, append.it)
+    }
+    
+    x.cast[,c('cell', unique(fia.trans$PalEON)[!is.na(unique(fia.trans$PalEON))])]
+  }
+  
+  agg_dens <- re.agg(fia.dens)
+  
+  colnames(agg_dens) <- tolower(gsub(' |[[:punct:]]', '\\.', colnames(agg_dens)))
+  colnames(agg_dens)[regexpr('poplar', colnames(agg_dens))>0] <- 'poplar'
+  agg_dens
+}
 
-pls.data <- read.csv('data/plss_density_alb_v0.9-4.csv', row.names=1)
-colnames(pls.data) <- tolower(colnames(pls.data))
-
-colnames(agg.dens) <- tolower(gsub(' ', '.', colnames(agg.dens), fixed=TRUE))
-
-colnames(agg.dens) <- gsub('/', '.', colnames(agg.dens), fixed=TRUE)
-
-agg.dens$poplar <- agg.dens$poplar.tulip.poplar
-
-best.taxa <- c('beech', 'ironwood', 'hemlock', 'fir', 'cedar.juniper', 
-              'basswood', 'spruce', 'elm', 'ash', 'pine', 'oak', 
-              'tamarack', 'maple', 'poplar', 'birch')
-
-agg.dens <- agg.dens[,c('cell', best.taxa)]
-pls.data <- pls.data[,c('cell', best.taxa)]
-
-agg.dens[is.na(agg.dens)] <- 0
-
-cell.xy <- data.frame(cell = 1:ncell(unit.raster),
-                     xyFromCell(unit.raster, 1:ncell(unit.raster)))
+get_pls <- function(version){
+  pls.data <- read.csv('data/plss_density_alb_v0.9-4.csv', row.names=1)
+  colnames(pls.data) <- tolower(colnames(pls.data))
+  
+  colnames(agg.dens) <- tolower(gsub(' ', '.', colnames(agg.dens), fixed=TRUE))
+  
+  colnames(agg.dens) <- gsub('/', '.', colnames(agg.dens), fixed=TRUE)
+  
+  agg.dens$poplar <- agg.dens$poplar.tulip.poplar
+  
+  best.taxa <- c('beech', 'ironwood', 'hemlock', 'fir', 'cedar.juniper', 
+                'basswood', 'spruce', 'elm', 'ash', 'pine', 'oak', 
+                'tamarack', 'maple', 'poplar', 'birch')
+  
+  pls.data[,c('cell', best.taxa)]
+}
