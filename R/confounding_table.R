4943c1eaaadef4d95291a4b44552d2ae3a29b75a
Simon <simon.j.goring@gmail.com>
2016-06-17 00:06:52 -0700

    Mostly `lint`ing, adding some p adjustment for the estimates and cleaning up the table output so that it shows up as symbols rather than values.

--- R/confounding_table.R
+++ R/confounding_table.R
@@ -13,7 +13,7 @@
 taxa <- unique(vegclim_table$taxon)
 clim_var <- unique(vegclim_table$climate)
 
-conf_test <- function(taxa, clim_var){
+conf_test <- function(taxa, clim_var) {
   clim = t.test(subset(vegclim_table, taxon == taxa & climate == clim_var & base == 'PLSS' & c.ref == 'PLSS' & data > 0)$clim,
          subset(vegclim_table, taxon == taxa & climate == clim_var & base == 'PLSS' & c.ref == 'FIA' & data > 0)$clim)
 
@@ -28,21 +28,22 @@ conf_test <- function(taxa, clim_var){
 }
 
 tests <- do.call(rbind.data.frame,
-        lapply(taxa, function(x){
-  do.call(rbind.data.frame,lapply(clim_var, function(y){conf_test(x, y)}))}))
+        lapply(taxa, function(x) {
+  do.call(rbind.data.frame,lapply(clim_var, function(y) {conf_test(x, y)}))}))
 
-checker <- function(x){
+checker <- function(x) {
   # x will be the row:
   # compounding:
   # both ps are sig & both ests are in the same direction:
-  if(as.numeric(x[4]) < (0.05 / 120) & as.numeric(x[6]) < (0.05 / 120)){
-    if((as.numeric(x[3]) * as.numeric(x[5])) > 0){
+  if (p.adjust(as.numeric(x[4]), method = "bonferroni", n = 120) < 0.05 & 
+      p.adjust(as.numeric(x[6]), method = "bonferroni", n = 120) < 0.05) {
+    if ((as.numeric(x[3]) * as.numeric(x[5])) > 0) {
       return('+')
     } else {
       return('-')
     }
   } else {
-    if(as.numeric(x[4]) > (0.05 / 120)){
+    if (as.numeric(x[4]) > (0.05 / 120)) {
       return('n/a')
     } else {
       return('.')
@@ -55,16 +56,27 @@ conf_table <- dcast(tests, taxa ~ clim, value.var = 'result')
 
 conf_table <- conf_table[match(taxa, conf_table$taxa),]
 
-mean_y <- function(taxa){
+mean_y <- function(taxa) {
   mean(subset(vegclim_table, taxon == taxa & base == 'PLSS' & data > 0)$y)
 }
 
-mean_x <- function(taxa){
+mean_x <- function(taxa) {
   mean(subset(vegclim_table, taxon == taxa & base == 'PLSS' & data > 0)$x)
 }
 
 conf_table$mean_y <- sapply(taxa, mean_y)
 conf_table$mean_x <- sapply(taxa, mean_x)
 
-conf_table <- conf_table[order(conf_table$mean_x),]
+conf_table <- conf_table[,1:5]
 
+conf_cor <- conf_table[,2:5]
+conf_cor[conf_cor == '+'] <- 1
+conf_cor[conf_cor == '.'] <- 0
+conf_cor[conf_cor == '-'] <- -1
+
+rownames(conf_cor) <- conf_table[,1]
+cluster <- hclust(dist(conf_cor))
+
+# Sort the table by similarity:
+conf_table <- conf_table[cluster$order, ]
+rownames(conf_table) <- NULL
